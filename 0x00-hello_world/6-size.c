de1e6bd554b52af606cf79609cb534485b9f7d1c
akins-dev <bobemmanuel908@gmail.com>
2022-12-08 17:12:18 -0800

    modified

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 ( success )
+ * Return: Always 0 (success)
+ *
  */
 
 int main(void)
@@ -19,5 +20,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));	
+
 	return (0);
 }
