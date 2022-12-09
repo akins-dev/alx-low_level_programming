08f9e1428bd663ba9bbb46cdafe607438ba8e655
akins-dev <bobemmanuel908@gmail.com>
2022-12-08 16:37:54 -0800

    Created

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 ( success )
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
