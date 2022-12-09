96fb3a4795fcea1fa0bd34472f5b05259d196217
akins-dev <bobemmanuel908@gmail.com>
2022-12-08 18:51:51 -0800

    Hot Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, quo, 59);
 	return (1);
 }
