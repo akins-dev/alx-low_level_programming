b8c5e04b66ae64109c2a53b0b67e4cfbdde4b0f2
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 20:41:55 -0800

    Created

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+																				}
+			++digit2;
+		}
+		++digit1;
+	}
+	putchar('\n');
+	return (0);
+}
