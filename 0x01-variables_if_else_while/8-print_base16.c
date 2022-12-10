ec28eb2270206a09d9542ee04b4ec99bce7381c1
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 19:41:48 -0800

    c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *  Return: Always 0 (success)
+ * Return: Always 0 (success)
  */ 
 
 int main(void)
@@ -21,5 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-
 }
