b83e50b81d861768c0a32380d5376724538d1775
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 19:37:53 -0800

    Created

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */ 
+
+int main(void)
+{
+	int digit = 48; /*48; decimal rep of 0*/
+
+	while (digit <= 102) /*102; decimal rep of f*/
+	{
+		putchar(digit);
+		/* after 9 we jump till 96; */
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+	return (0);
+
+}
