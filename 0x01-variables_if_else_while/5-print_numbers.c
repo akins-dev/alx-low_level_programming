e184d6ee96d7c1ea8b464b53d5189762d1ea6ff6
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 19:07:20 -0800

    Created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		++num;
+	}
+	printf("\n");
+	return (0);
+}
