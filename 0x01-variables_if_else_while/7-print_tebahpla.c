54a7f38f703f24d290271369f47471ed5b08b604
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 19:21:44 -0800

    Created 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		++ch;
+	}
+	putchar('\n');
+	return (0);
+}
