6ba6170784507f3628908df3fc7dc640f1abdc68
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 18:46:00 -0800

    Created 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch, ch2 = "a", "A";
+
+	while (ch <= "z")
+	{
+		putchar(ch);
+		++ch;
+	}
+	while (ch2 <= "Z")
+	{
+		putchar(ch2);
+		++ch2;
+	}
+	putchar("\n");
+}
