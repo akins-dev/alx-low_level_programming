f4dd2cf6700f65cd761b647d16985c818c6c13ea
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 19:12:17 -0800

    Created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		++num;
+	}
+	putchar('\n');
+	return (0);
+}
