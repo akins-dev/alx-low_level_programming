cdd26d272263a26bfde8b634eca7dedd0a545c57
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 18:59:27 -0800

    Created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
