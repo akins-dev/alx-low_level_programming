08c4c32da28ac321851d11f0f7b551da3a1d41af
akins-dev <bobemmanuel908@gmail.com>
2022-12-09 18:36:46 -0800

    Created

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch;
+	}
+		putchar('\n');
+	return (0);
+}
