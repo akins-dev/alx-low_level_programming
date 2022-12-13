02bfadd9f70824fb91337e3daee6df060427305c
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 12:38:48 -0800

    Created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.c"
+
+/**
+ * print_alphabet - prints a-z
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+
+	return;
+}
