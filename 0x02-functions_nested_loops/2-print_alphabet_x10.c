0536ee0be30c6994c7966ae0787ee64f15f53b7e
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 13:04:17 -0800

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10 - prints a-z ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	char n = 'a';
+	int count = 0;
+
+	while (count < 10)
+	{
+		while (n <= 'z')
+		{
+			_putchar(n);
+			n++;
+		}
+		n = 'a';
+		_putchar('\n');
+		count++;
+	}
+	return;
+}
