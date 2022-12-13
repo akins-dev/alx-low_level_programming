88bce9f71e861760afa1b3c1eb278763566e99ac
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 15:30:23 -0800

    Created

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _islower - function that checks if a letter is in upper or lowecase
+ *
+ * Return: 1 if letter is in lowercase and 0 otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 || c <= 122)
+	{
+		return 1;
+	}
+
+	return 0;
+}
