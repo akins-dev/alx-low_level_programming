2e281124e289fdebd7c9632b9cb875fd3e770c31
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 15:58:37 -0800

    Hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,19 @@
 
 int print_sign(int n)
 {
-	int out;
-
 	if (n > 0)
 	{
 		_putchar('+');
-		out = 1;
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		out = 0;
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-		out = -1;
+		return (-1);
 	}
-	return (out);
 }
