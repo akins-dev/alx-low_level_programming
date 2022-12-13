f1b98e733f56e95550ae8bd74e960ae19164726f
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 12:47:18 -0800

    Modified

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * main - check the code for Holberton School students.
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
