56def9926c82dc6070285b69871ee7c2100dfd66
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 18:26:27 -0800

    M

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_to_98 - print n to 98 count separated by comma, followed by space and number is be printed in order
+ *
+ * @n: stores input
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
+}
