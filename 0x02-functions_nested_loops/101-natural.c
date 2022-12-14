d740e055a5d6701d407bb0ac7bd4617aa319bfc9
akins-dev <bobemmanuel908@gmail.com>
2022-12-13 19:11:23 -0800

    M

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,20 +10,12 @@ int main(void)
 {
 	int i, count;
 
-	for (i = 0; i < 1024; i--)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			count += i;
-		else if ((i % 5) == 0)
-			count += i;
-		else
-			count += 0;
 	}
-	/* Remove the last digit and recur */
-	if (count/10)
-		_putchar(count/10 + '0');
-	/* Print the last digit */
-	_putchar(count%10 + '0');
+	printf("%d\n", count);
 
 	return (0);
 }
