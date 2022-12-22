5cc8d5e7aa4cc012fce4cdfd5264d21cf4d2d580
akins-dev <bobemmanuel908@gmail.com>
2022-12-22 12:00:35 -0800

    Created

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,14 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 	int c, i;
 
 	c = 0;
+
 	/*find size of dest array*/
 	while (dest[c])
 		c++;
 
 	/**
 	 * src does not need to be null terminated
- 	 * if it contains n or more bytes
-	*/
+	 * if it contains n or more bytes
+	 */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
 	/*null terminate dest*/
