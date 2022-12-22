907e7193141d44c27a8d9d6e764237d16134c156
akins-dev <bobemmanuel908@gmail.com>
2022-12-22 12:30:19 -0800

    Created

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,46 @@
 /**
- * _strcat - function that concatenates
- *          two strings.
+ * _strcmp - a function that compares two strings
  *
- * @dest: pointer to destnation input
- * @src: pointer to source input
+ * @s1: string 1 input to compare
+ * @s2: against this other string 2
  *
- * Return: pointer to resulting string @dest
+ * Return: 0 if s1 and s2 are equal
+ *        negative integer if the stopping character
+ *                in @s1 was less than the stopping
+ *                character in @s2
+ *        positive integer if the stopping character
+ *                in @s1 was greater than the stopping
+ *                character in @s2
  */
 
-char *_strcat(char *dest, char *src)
+int _strcmp(char *s1, char *s2)
 {
-	int c, c2;
+	int i = 0, r;
 
-	c = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-		c++;
-	/* iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2] ; c2++)
-	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-		dest[c++] = src[c2];
-	return (dest);
+	/**
+ 	 * iterate through src and
+ 	 * compare it with dest
+	 */
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		/**
+		 * if they differ by a single character
+		 * don't iterate further
+		 */
+		if (s1[i] != s2[i])
+		{
+		/**
+		 * return the difference between
+		 * the two characters
+		 */
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
+	}
+	return (r);
 }
