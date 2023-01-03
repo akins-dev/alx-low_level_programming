e66f4afc8a5851a6d3b043af68e488f730489437
akins-dev <bobemmanuel908@gmail.com>
2023-01-02 18:54:57 -0800

    Minor update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,12 @@
 /*
  * _memset: fills the first n bytes of the memory area pointed to by s with the constant byte b
  *
+ * @*s: a pointer, pointing to the memory area to be filled
+ *
+ * @b: the constant byte to fill the memory area with
+ *
+ * @n: the amount of bytes of the memory area to fill
+ *
  * return:  a pointer to the memory area s
  */
 
