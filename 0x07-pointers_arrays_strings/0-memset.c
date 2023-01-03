7f407164a0e9d16adeedffc40374131634a7c052
akins-dev <bobemmanuel908@gmail.com>
2023-01-02 18:51:07 -0800

    Updates

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,13 +9,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	char buffer[n];
-
-	buffer = &s;
 	
 	for (i = 0; i < n; i++)
 	{
-		buffer[i] = b;
+		s[i] = b;
 	}
-	return (buffer);
+	return (s);
 }
