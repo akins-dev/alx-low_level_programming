d31c6097628c643538c4ae955c27f716c512a397
akins-dev <bobemmanuel908@gmail.com>
2023-01-03 16:39:58 -0800

    Created

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr:  locates a character in a string
+ * 	and returns a pointer to the first ooccurence
+ * 	of the character. It reurns NULL if not found.
+ *
+ * @s: A pointer to a string
+ * @c: Character to be located
+ *
+ * Return: A pointer to the first occurence of 
+ * 	a character, NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	       if (s[i] == c)
+		       return (s[i]);
+		i++;
+	return (NULL);
+}
