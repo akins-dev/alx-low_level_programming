bac39cdb04c8b24588af89486097d2b1a1dfb32f
akins-dev <bobemmanuel908@gmail.com>
2023-01-03 16:58:42 -0800

    Updates

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strchr:  locates a character in a string
  * 	and returns a pointer to the first ooccurence
@@ -12,11 +14,13 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (*(s + i) != '\0')
-	       if (*(s + i) == c)
-		       return (*(s + i));
-		i++;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		       return (s[i]);
+	}
+
 	return ('\0');
 }
