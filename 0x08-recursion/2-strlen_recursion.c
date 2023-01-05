820efaefd6a1137c1d338124148c7465009c9a20
akins-dev <bobemmanuel908@gmail.com>
2023-01-04 19:50:28 -0800

    Created

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that prints a new line
+ *
+ * @s: the string
+ *
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return ( 1 + (_strlen_recursion(s + 1)));
+	}
+	else
+		return (0);
+}
