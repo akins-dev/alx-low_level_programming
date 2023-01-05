7231dc8a2663550a0efa46ab89186d0113cd24e0
akins-dev <bobemmanuel908@gmail.com>
2023-01-04 18:32:25 -0800

    Updates

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,8 @@
 
 void _puts_recursion(char *s)
 {
-	if ( s[0] == '\0')
-		_putchar(s[0]);
-		return;
 	_putchar(s[0]);
-	_puts_recursion(s + 1);
+	_putchar(s[0]);
+	if ( s[1] )
+		_puts_recursion(s + 1);
 }
