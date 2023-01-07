2aee40eeea9610fcd42687786ea3ff8121f29e4c
akins-dev <bobemmanuel908@gmail.com>
2023-01-06 19:50:42 -0800

    created: 0-whatsmyname and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: Argument vector (array of pointers to char)
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
