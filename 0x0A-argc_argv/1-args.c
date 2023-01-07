e7cdfec47f6b01db1244aa4ab981c487d02faff0
akins-dev <bobemmanuel908@gmail.com>
2023-01-06 19:56:38 -0800

    Created: 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: Argument vector (array of pointers to char)
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
