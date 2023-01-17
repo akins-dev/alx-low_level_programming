f9dfa97f877e8a88088718b5b826efa558cdb98f
akins-dev <bobemmanuel908@gmail.com>
2023-01-17 14:57:24 -0800

    Betty fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,13 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *s;
 
-
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 
 	/*find length of str1 & str2*/
 	while (s1[a] != '\0')
@@ -29,14 +26,11 @@ char *str_concat(char *s1, char *s2)
 	while (s2[b] != '\0')
 		b++;
 
-
 	/*+1 for our end of string character*/
 	s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
 
-
 	if (s == NULL)
 		return (NULL);
-
 
 	/*add the first string to array s*/
 	for (i = 0; s1[i] != '\0'; i++)
@@ -49,10 +43,8 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	}
 
-
 	/*null terminate our new string*/
 	s[i] = '\0';
-
 
 	return (s);
 }
