d9b01d9db7c3e0a1076878069eded800f654f2f7
akins-dev <bobemmanuel908@gmail.com>
2023-01-17 15:05:02 -0800

    updates

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-		grid = malloc(height * sizeof(int *));
+	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
