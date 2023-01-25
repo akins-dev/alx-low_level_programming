79530ba59f51b9a6e78c67261d9219c8e1b4893d
akins-dev <emmanuelakins503@gmail.com>
2023-01-24 19:05:46 -0800

    updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int index;
 
 	if (array != NULL && size > 0 && action != NULL)
+	{
 		for (index = 0; index < size; index++)
 			action(array[index]);
+	}
 }
